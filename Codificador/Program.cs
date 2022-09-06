440642ac43162539c820d1a65194cbf93a52e24b
Edu Andrade <eduardo.a@edu.pucrs.br>
2022-09-06 07:35:40 -0300

    Funcionando nrzi

---
+++ Codificador/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace Codificador;
+
+public class Program
+{
+    private static readonly Dictionary<char, string> HexCharToBin = new()
+    {
+        { '0', "0000" },
+        { '1', "0001" },
+        { '2', "0010" },
+        { '3', "0011" },
+        { '4', "0100" },
+        { '5', "0101" },
+        { '6', "0110" },
+        { '7', "0111" },
+        { '8', "1000" },
+        { '9', "1001" },
+        { 'a', "1010" },
+        { 'b', "1011" },
+        { 'c', "1100" },
+        { 'd', "1101" },
+        { 'e', "1110" },
+        { 'f', "1111" }
+    };
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("erro");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "nrzi":
+                Console.WriteLine(nrziEncode(args[1]));
+                break;
+            case "mdif":
+            case "hdb3":
+            case "8b6t":
+            case "6b8b":
+                Console.WriteLine($"Selected {args[0]} with data {args[1]}");
+                break;
+            default:
+                Console.WriteLine("erro");
+                break;
+        }
+    }
+
+    public static string nrziEncode(string hexInput)
+    {
+        var encodedData = new StringBuilder();
+        var signal = '-';
+        foreach (var c in hexInput)
+        {
+            foreach (var bin in HexCharToBin[c])
+            {
+                if (bin == '1')
+                {
+                    signal = signal switch
+                    {
+                        '-' => '+',
+                        '+' => '-'
+                    };
+                }
+                encodedData.Append(signal);
+            }
+        }
+
+        return encodedData.ToString();
+    }
+}
---
+++ Decodificador/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace Decodificador;
+
+public class Program
+{
+    private static readonly Dictionary<string, char> BinToHexChar = new()
+    {
+        { "0000", '0' },
+        { "0001", '1' },
+        { "0010", '2' },
+        { "0011", '3' },
+        { "0100", '4' },
+        { "0101", '5' },
+        { "0110", '6' },
+        { "0111", '7' },
+        { "1000", '8' },
+        { "1001", '9' },
+        { "1010", 'a' },
+        { "1011", 'b' },
+        { "1100", 'c' },
+        { "1101", 'd' },
+        { "1110", 'e' },
+        { "1111", 'f' }
+    };
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("erro");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "nrzi":
+                Console.WriteLine(nrziDecode(args[1]));
+                break;
+            case "mdif":
+            case "hdb3":
+            case "8b6t":
+            case "6b8b":
+                Console.WriteLine($"Selected {args[0]} with data {args[1]}");
+                break;
+            default:
+                Console.WriteLine("erro");
+                break;
+        }
+    }
+
+    public static string nrziDecode(string binInput)
+    {
+        var decodedData = new StringBuilder();
+        var digit = 0;
+        var lastSignal = '-';
+        for (var i = 0; i <= binInput.Length - 4; i += 4)
+        {
+            var hex = new StringBuilder();
+            foreach (var c in binInput[i..(i + 4)])
+            {
+                digit = c != lastSignal ? 1 : 0;
+
+                lastSignal = c;
+                hex.Append(digit);
+            }
+
+            hex.Append(' ');
+            decodedData.Append(hex);
+        }
+
+        return decodedData.ToString();
+    }
+}

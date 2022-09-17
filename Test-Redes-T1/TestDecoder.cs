5eed153ae24546d8d3f9f0aad5ca70137955e960
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2022-09-17 16:31:48 -0300

    Adjusted unit tests for decoder

--- Test-Redes-T1/TestDecoder.cs
+++ Test-Redes-T1/TestDecoder.cs
@@ -1,4 +1,4 @@
-﻿using Codificador;
+﻿using Decodificador;
 
 namespace Test_Redes_T1;
 
@@ -6,106 +6,106 @@ public class TestDecoder
 {
     private static readonly TestResult[] ExpectedNrzi =
     {
-        new("5678", "-++--+---+-+----"),
-        new("ABCDEF", "++--++-+-+++-++-+-++-+-+"),
-        new("2468AC", "--+++----+--++++--++-+++"),
-        new("13579B", "---+++-++--++-+-+++-++-+"),
-        new("0123456789AB", "-------+++----+--++++--++-+++-+-++++---+--++--+-"),
-        new("AABBCCDDEEFF", "++--++--++-+--+-+---+---+--+-++-+-++-+--+-+-+-+-"),
+        new("-++--+---+-+----", "5678"),
+        new("++--++-+-+++-++-+-++-+-+", "ABCDEF"),
+        new("--+++----+--++++--++-+++", "2468AC"),
+        new("---+++-++--++-+-+++-++-+", "13579B"),
+        new("-------+++----+--++++--++-+++-+-++++---+--++--+-", "0123456789AB"),
+        new("++--++--++-+--+-+---+---+--+-++-+-++-+--+-+-+-+-", "AABBCCDDEEFF"),
     };
 
     private static readonly TestResult[] ExpectedMdif =
     {
-        new("5678", "+--+-++-+--++-+-+--++--++-+-+-+-"),
-        new("ABCDEF", "-+-++-+--+-++--++--+-+-++--+-++--++--+-++--++--+"),
-        new("2468AC", "+-+--+-+-++-+-+-+--++-+--+-+-+-++-+--+-++--+-+-+"),
-        new("13579B", "+-+-+--+-+-++--+-++-+--+-++--++--+-+-++--+-++--+"),
-        new("0123456789AB",
-            "+-+-+-+-+-+-+--+-+-++-+-+-+--++-+--+-+-+-++-+--+-++--+-+-++--++--+-+-+-++-+-+--++-+--+-++-+--++-"),
-        new("AABBCCDDEEFF",
-            "-+-++-+--+-++-+--+-++--++-+--++--++-+-+--++-+-+--++-+--++--+-++--++--+-++--++-+--++--++--++--++-"),
+        new("+--+-++-+--++-+-+--++--++-+-+-+-", "5678"),
+        new("-+-++-+--+-++--++--+-+-++--+-++--++--+-++--++--+", "ABCDEF"),
+        new("+-+--+-+-++-+-+-+--++-+--+-+-+-++-+--+-++--+-+-+", "2468AC"),
+        new("+-+-+--+-+-++--+-++-+--+-++--++--+-+-++--+-++--+", "13579B"),
+        new("+-+-+-+-+-+-+--+-+-++-+-+-+--++-+--+-+-+-++-+--+-++--+-+-++--++--+-+-+-++-+-+--++-+--+-++-+--++-",
+            "0123456789AB"),
+        new("-+-++-+--+-++-+--+-++--++-+--++--++-+-+--++-+-+--++-+--++--+-++--++--+-++--++-+--++--++--++--++-",
+            "AABBCCDDEEFF"),
     };
 
     private static readonly TestResult[] Expected8B6T =
     {
-        new("5678", "--++0+---++0"),
-        new("ABCDEF", "+-+--++0-+00--++0-"),
-        new("2468AC", "+-0+000--+-+-++--+"),
-        new("13579B", "-0-++0--+++0-+-00+"),
-        new("0123456789AB", "0-+-+0+-0-++00+0--++000-0-0+00+-+--+"),
-        new("AABBCCDDEEFF", "+-+-+-0+0--+-+000++0--00++-0+-00-+0-"),
+        new("--++0+---++0", "5678"),
+        new("+-+--++0-+00--++0-", "ABCDEF"),
+        new("+-0+000--+-+-++--+", "2468AC"),
+        new("-0-++0--+++0-+-00+", "13579B"),
+        new("0-+-+0+-0-++00+0--++000-0-0+00+-+--+", "0123456789AB"),
+        new("+-+-+-0+0--+-+000++0--00++-0+-00-+0-", "AABBCCDDEEFF"),
     };
 
     private static readonly TestResult[] Expected6B8B =
     {
-        new("5678", "+++--++---+++-+-+-++++--+++-++--"),
-        new("ABCDEF", "++--++----+-+----++--+---+--+---"),
-        new("2468AC", "+---+++-+----+--+-++++--++--+---"),
-        new("13579B", "-+---++---+--++----+-+-----+--+-"),
-        new("0123456789AB", "-++-+++-+--+++--++++-++-+----++-+++-+++-++-+----++---+----++--+-"),
-        new("AABBCCDDEEFF", "++--++----++--+--+--+---+---+----++--+-----+-+---++-++---+---+--"),
+        new("+++--++---+++-+-+-++++--+++-++--", "5678"),
+        new("++--++----+-+----++--+---+--+---", "ABCDEF"),
+        new("+---+++-+----+--+-++++--++--+---", "2468AC"),
+        new("-+---++---+--++----+-+-----+--+-", "13579B"),
+        new("-++-+++-+--+++--++++-++-+----++-+++-+++-++-+----++---+----++--+-", "0123456789AB"),
+        new("++--++----++--+--+--+---+---+----++--+-----+-+---++-++---+---+--", "AABBCCDDEEFF"),
     };
 
     private static readonly TestResult[] ExpectedHdb3 =
     {
-        new("5678", "0+0-0+-00+-+-000"),
-        new("ABCDEF", "+0-0+0-+-+00-+0-+-+0-+-+"),
-        new("2468AC", "00+00-000+-0+000-0+0-+00"),
-        new("13579B", "000+00-+0-0+0-+-+00-+0-+"),
-        new("0123456789AB", "+00+000-00+000-+0-000+0-0+-00+-+-000+00-+0-0+0-+"),
-        new("AABBCCDDEEFF", "+0-0+0-0+0-+-0+-+-00+-00+-0+-+0-+-+0-+-0+-+-+-+-"),
+        new("0+0-0+-00+-+-000", "5678"),
+        new("+0-0+0-+-+00-+0-+-+0-+-+", "ABCDEF"),
+        new("00+00-000+-0+000-0+0-+00", "2468AC"),
+        new("000+00-+0-0+0-+-+00-+0-+", "13579B"),
+        new("+00+000-00+000-+0-000+0-0+-00+-+-000+00-+0-0+0-+", "0123456789AB"),
+        new("+0-0+0-0+0-+-0+-+-00+-00+-0+-+0-+-+0-+-0+-+-+-+-", "AABBCCDDEEFF"),
     };
 
     [Test]
-    public void TestEncoderNrzi()
+    public void TestDecoderNrzi()
     {
         foreach (var testExpected in ExpectedNrzi)
         {
-            string testOutput = Encoder.EncodeNrzi(testExpected.Input);
+            string testOutput = Decoder.DecodeNrzi(testExpected.Input);
             Assert.That(testOutput, Is.EqualTo(testExpected.Output),
                 () => $"For nrzi with input {testExpected.Input}:");
         }
     }
 
     [Test]
-    public void TestEncoderMdif()
+    public void TestDecoderMdif()
     {
         foreach (var testExpected in ExpectedMdif)
         {
-            string testOutput = Encoder.EncodeMdif(testExpected.Input);
+            string testOutput = Decoder.DecodeMdif(testExpected.Input);
             Assert.That(testOutput, Is.EqualTo(testExpected.Output),
                 () => $"For mdif with input {testExpected.Input}:");
         }
     }
 
     [Test]
-    public void TestEncoder8B6T()
+    public void TestDecoder8B6T()
     {
         foreach (var testExpected in Expected8B6T)
         {
-            string testOutput = Encoder.Encode8B6T(testExpected.Input);
+            string testOutput = Decoder.Decode8B6T(testExpected.Input);
             Assert.That(testOutput, Is.EqualTo(testExpected.Output),
                 () => $"For 8b6t with input {testExpected.Input}:");
         }
     }
 
     [Test]
-    public void TestEncoder6B8B()
+    public void TestDecoder6B8B()
     {
         foreach (var testExpected in Expected6B8B)
         {
-            string testOutput = Encoder.Encode6B8B(testExpected.Input);
+            string testOutput = Decoder.Decode6B8B(testExpected.Input);
             Assert.That(testOutput, Is.EqualTo(testExpected.Output),
                 () => $"For 6b8b with input {testExpected.Input}:");
         }
     }
 
     [Test]
-    public void TestEncoderHdb3()
+    public void TestDecoderHdb3()
     {
         foreach (var testExpected in ExpectedHdb3)
         {
-            string testOutput = Encoder.EncodeHdb3(testExpected.Input);
+            string testOutput = Decoder.DecodeHdb3(testExpected.Input);
             Assert.That(testOutput, Is.EqualTo(testExpected.Output),
                 () => $"For Hdb3 with input {testExpected.Input}:");
         }
